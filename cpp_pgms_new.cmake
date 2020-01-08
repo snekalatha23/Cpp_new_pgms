8d9bcceee6b28f8d0579defb238de4e3b2d7ab91
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2020-01-08 12:04:31 +0530

    Update cpp_pgms_new.cmake

--- cpp_pgms_new.cmake
+++ cpp_pgms_new.cmake
@@ -1,2 +1,9 @@
 cmake_minimum_required(VERSION 2.6)
-	add_executable(exceptionCpp exceptionHandl.cpp)
+	add_executable(exceptionCpp exceptionHandl.cpp
+	assigntopCpp assignmentop.cpp
+	bitwiseop bitwiseop.cpp
+	catchCpp catch.cpp
+	conditionalCpp conditionalCpp.cpp
+	copyconstCpp copy_const.cpp
+	privateCpp privateCpp.cpp
+	publicCpp publicCpp.cpp)
